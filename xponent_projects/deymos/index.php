b1ace29b09995d0584593fbf86a82c7b5a42ec0f
Winston Ilag <winston.ilag@xponent.com.au>
2011-06-27 21:09:26 +1000

    our Xponent directory, main

---
+++ xponent_projects/deymos/index.php
@@ -0,0 +1,73 @@
+<?php
+
+#  PHP Coding....
+
+?>
+
+<HTML>
+<HEAD>
+
+	<link rel="stylesheet" type="text/css" href="/public/web/css/reset.css" />
+	<link rel="stylesheet" type="text/css" href="/public/web/css/text.css" />
+	<link rel="stylesheet" type="text/css" href="/public/web/css/960.css" />
+
+	<script type="text/javascript" src="/public/web/js/jquery-1.5.1.min.js"></script>
+
+</HEAD>
+<BODY>
+
+	<div class="container_12">
+	
+	<div class="grid_2 push_1">
+	
+		Logo
+	</div>
+	
+	
+	<div class="grid_6 push_4">
+	<h1>
+	Deymos Accountancy
+	</h1>
+	</div>
+	
+	
+	
+	<div class="clear"></div>
+	
+	  <div class="grid_7" style="background-color:blue;height:400px;">
+	    <p>
+	      Carousel goes here
+	    </p>
+	  </div>
+	  	  
+	  <div class="grid_5" style="background-color:cyan">
+	    <p>
+		Descriptions go here
+	    </p>
+	  </div>
+
+
+	<div class="clear"></div>
+
+
+	<div class="grid_4" style="background-color:yellow;">
+		Product 1
+	</div>
+
+	<div class="grid_4" style="background-color:lightgray">
+		Product 2
+	</div>
+
+	<div class="grid_4" style="background-color:green;">
+		Product 3
+	</div>
+
+
+		
+	</div>
+</BODY>
+</HTML>
+
+
+
+
